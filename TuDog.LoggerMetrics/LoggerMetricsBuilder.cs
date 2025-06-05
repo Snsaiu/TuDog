e7ce7728cface10369d4e61f9cbe2c6568a92d1f
Saiu <SNsaiu@outlook.com>
2025-06-05 18:21:43 +0800

    更换AddLoggerBuilder方法可以自定义文件路径；添加中央包管理

--- TuDog.LoggerMetrics/LoggerMetricsBuilder.cs
+++ TuDog.LoggerMetrics/LoggerMetricsBuilder.cs
@@ -8,15 +8,15 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class LoggerMetricsBuilder
 {
-    public static void AddLoggerBuilder(this IServiceCollection services, string lokiUrl, IEnumerable<LokiLabel>? lokiLabels,string appName)
+    public static void AddLoggerBuilder(this IServiceCollection services, string lokiUrl, IEnumerable<LokiLabel>? lokiLabels, string logDirectory)
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-        var logDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"YouYan", appName, "Logs");
-        Directory.CreateDirectory(logDir);
+        if (!Directory.Exists(logDirectory))
+            Directory.CreateDirectory(logDirectory);
 
-        var logPath = Path.Combine(logDir, "log-.txt");
-        
+        var logPath = Path.Combine(logDirectory, "log-.txt");
+
         var logger = new LoggerConfiguration()
             .MinimumLevel.Debug()
             .Enrich.FromLogContext()
