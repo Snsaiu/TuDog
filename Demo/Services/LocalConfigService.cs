2a6a21dc4108d88c130298e925f1831c217763c2
Saiu <SNsaiu@outlook.com>
2026-06-26 11:38:28 +0800

    TUDOG-4 TUDOG 可以配置本地配置文件名称

---
+++ Demo/Services/LocalConfigService.cs
@@ -0,0 +1,14 @@
+﻿using TuDog.Interfaces.LocalPreferences;
+using TuDog.Interfaces.PreferenceServices.Impl;
+using TuDog.IocAttribute;
+
+namespace Demo.Services;
+
+public interface INameConfigService : ILocalConfigService<string>;
+
+[Register<INameConfigService>]
+public sealed class NameConfigService : LocalConfigService<string>, INameConfigService
+{
+    public override string Key => "name";
+    public override string Default => "demo";
+}
